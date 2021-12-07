7aa9a6d25c2bc3ba7912b2607e86733be81f2930
gsmafa <gulom.1974@gmail.com>
2021-12-07 14:07:51 +0500

    created blocks newsletter and about-us also created _newsletter.sass and _about-us.sass, added styles and media style for them

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-/*------------ Slider Swiper ------------*/
-const swiper = new Swiper('.swiper', {
+/*------------ Slider Swiper Block Hotel ------------*/
+const hotelSlider = new Swiper('.hotel__slider', {
+  // Optional parameters
+  loop: true,
+	keyboard: {
+		enabled: true,
+		pageUpDown: true,
+	},
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button--next',
+    prevEl: '.swiper-button--prev',
+  },
+});
+/*------------ Slider Swiper Block About Us ------------*/
+const aboutusSlider = new Swiper('.about-us__slider', {
   // Optional parameters
   loop: true,
 	keyboard: {
