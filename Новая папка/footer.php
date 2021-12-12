7c37b3fd55ecb7da82c086e1ace5856414e65ab7
gsmafa <gulom.1974@gmail.com>
2021-12-12 16:15:43 +0500

    created file send.php & try send message through contact form in footer, got success

---
+++ Новая папка/footer.php
@@ -0,0 +1,90 @@
+<footer class="footer">
+		<div class="container footer-wrapper">
+					<img src="image/vertical_logo.svg" alt="Logo: Best Tour Plan" class="footer__logo">
+				<div class="footer__categories">
+					<h3 class="footer__title">All categories</h3>
+					<ul class="footer__list">
+						<li class="footer__item"><a href="#" class="footer__link">All Deals</a></li>
+						<li class="footer__item"><a href="#" class="footer__link">Hotels</a></li>
+						<li class="footer__item"><a href="#" class="footer__link">Activities</a></li>
+						<li class="footer__item"><a href="#" class="footer__link">Spa Packages</a></li>
+						<li class="footer__item"><a href="#" class="footer__link">Hotel Day Packages</a></li>
+						<li class="footer__item"><a href="#" class="footer__link">Restaurants</a></li>
+						<li class="footer__item"><a href="#" class="footer__link">Fitness</a></li>
+						<li class="footer__item"><a href="#" class="footer__link">Rodrigues</a></li>
+					</ul>
+				</div>
+				<div class="footer__additional">
+					<h3 class="footer__title">Additional information</h3>
+					<ul class="footer__list">
+						<li class="footer__item"><a href="#" class="footer__link">About Us</a></li>
+						<li class="footer__item"><a href="#" class="footer__link">Contact Us</a></li>
+						<li class="footer__item"><a href="#" class="footer__link">How does it work?</a></li>
+						<li class="footer__item"><a href="#" class="footer__link">Frequently Asked Questions</a></li>
+						<li class="footer__item"><a href="#" class="footer__link">Deals.mu loyalty program</a></li>
+						<li class="footer__item"><a href="#" class="footer__link">Promote your Business on BTP</a></li>
+					</ul>
+				</div>
+				<div class="footer__social">
+					<h3 class="footer__title social-title">Social network</h3>
+					<div class="footer__social-icons">
+						<img src="image/facebook.svg" alt="Facebook" class="footer__icon">
+						<img src="image/youtube.svg" alt="Youtube" class="footer__icon">
+						<img src="image/instagram.svg" alt="Instagram" class="footer__icon">
+					</div>
+				</div>
+				<div class="footer__legal">
+					<h3 class="footer__title">Legal information</h3>
+					<ul class="footer__list">
+						<li class="footer__item"><a href="#" class="footer__link">Terms & Conditions</a></li>
+						<li class="footer__item"><a href="#" class="footer__link">Disclaimer</a></li>
+						<li class="footer__item"><a href="#" class="footer__link">Cancellation policy</a></li>
+						<li class="footer__item"><a href="#" class="footer__link">Privacy</a></li>
+					</ul>
+				</div>
+				<div class="footer__contacts">
+					<h3 class="footer__title">Contact details</h3>
+					<p class="footer__text">Feel free to contact us by phone, email or by our contact form</p>
+					<div class="footer__contact">
+						<img src="image/map-pin.svg" alt="Best Tour Plan: address" class="footer__contact-icon">
+						<span class="footer__address">9748 Blossom Hill Rd undefined Lansing, Idaho 68545 United States</span>
+					</div>
+					<div class="footer__contact">
+						<img src="image/phone-call.svg" alt="Best Tour Plan: phone" class="footer__contact-icon">
+						<span class="footer__phone">Tel (business hours) : 269 1500</span>
+						<span class="footer__phone">Tel (hotline) Monday - Saturday: 52-56-61-38 (08:00 am – 20:00 pm)</span>
+						<span class="footer__phone">Tel (hotline) Sunday: 52-56-61-38 (08:00 am – 14:00 pm)</span>
+					</div>
+					<div class="footer__contact">
+						<img src="image/email.svg" alt="Best Tour Plan: email" class="footer__contact-icon">
+						<span class="footer__email">cherly.lawson@example.com</span>
+					</div>
+				</div>
+				<div class="footer__message">
+					<h3 class="footer__title">Send us message</h3>
+					<form action="send.php" method="POST" class="footer__form">
+						<input type="text" class="footer__input" placeholder="Your Full Name*">
+						<input type="text" class="footer__input" placeholder="Phone Number*">
+						<textarea cols="30" rows="10" class="footer__textarea" placeholder="Message"></textarea>
+						<span class="footer__required">* Required Fields</span>
+						<button class="footer__button">Send</button>
+					</form>
+				</div>
+				<div class="footer__disclaimer">
+					<ul class="footer__list footer__disclaimer-list">
+						<li class="footer__item footer__disclaimer-item"><a href="#" class="footer__link">Disclaimer</a></li>
+						<li class="footer__item footer__disclaimer-item"><a href="#" class="footer__link">Conditions of use</a></li>
+						<li class="footer__item footer__disclaimer-item"><a href="#" class="footer__link">Cancellation policy</a></li>
+					</ul>
+				</div>
+				<div class="footer__cards">
+					<img src="image/cards-pay.png" alt="Cards pay" class="footer__cards-pay">
+				</div>
+		</div>
+	</footer>
+	<!-- /.footer -->
+	<script src="js/swiper-bundle.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js"></script>
+	<script src="js/script.js"></script>
+</body>
+</html>
---
+++ Новая папка/header.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Best Tour Plan Hotel Booking</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;700&family=Nunito:wght@400;700;800&display=swap"
+	rel="stylesheet">
+	<link rel="stylesheet" href="css/swiper-bundle.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header class="header">
+		<div class="container">
+			<div class="header-top">
+				<a href="#" class="logo">
+					<img src="image/logo_horizontal.svg" alt="Logo: Best Tour Plan" class="logo__image">
+				</a>
+				<form action="#" class="search header__search search__mobile--hidden">
+					<input type="text" class="search__input" placeholder="Search Location">
+					<button class="search__button">
+						<img src="image/search_icon.svg" alt="Icon: search" class="search__icon">
+					</button>
+				</form>
+				<a href="#" class="user user__mobile--hidden">
+					<img src="image/user_avatar.jpg" alt="Avatar: Natan" class="user__avatar">
+					<span class="user__name">Natan</span>
+				</a>
+				<button class="hamburger">
+					<span class="hamburger__line hamburger__line-top"></span>
+					<span class="hamburger__line hamburger__line-middle"></span>
+					<span class="hamburger__line hamburger__line-bottom"></span>
+				</button>
+			</div>
+			<!-- .header-top -->
+		</div>
+		<!-- /.container -->
+			<div class="header-menu header-menu__mobile--hidden">
+				<div class="container">
+					<nav class="menu">
+						<ul class="menu__list">
+							<li class="menu__item item-1 active"><a href="#" class="menu__link">All Deals</a></li>
+							<li class="menu__item item-2"><a href="#" class="menu__link">Hotels</a></li>
+							<li class="menu__item item-3"><a href="#" class="menu__link">Activities</a></li>
+							<li class="menu__item item-4"><a href="#" class="menu__link">Hotel Day Packages</a></li>
+							<li class="menu__item item-5"><a href="#" class="menu__link">Restaurants</a></li>
+							<li class="menu__item item-6"><a href="#" class="menu__link">Events</a></li>
+							<li class="menu__item item-7"><a href="#" class="menu__link">Rodrigues</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+			<!-- .header-menu -->
+			<div class="header-menu menu-mobile--hidden">
+				<div class="container">
+					<nav class="menu">
+						<a href="#" class="user">
+							<img src="image/user_avatar.jpg" alt="Avatar: Natan" class="user__avatar">
+							<span class="user__name">Natan</span>
+						</a>
+						<form action="#" class="search header__search">
+							<input type="text" class="search__input" placeholder="Search Location">
+							<button class="search__button">
+								<img src="image/search_icon.svg" alt="Icon: search" class="search__icon">
+							</button>
+						</form>
+						<ul class="menu__list">
+							<li class="menu__item item-1 active"><a href="#" class="menu__link">All Deals</a></li>
+							<li class="menu__item item-2"><a href="#" class="menu__link">Hotels</a></li>
+							<li class="menu__item item-3"><a href="#" class="menu__link">Activities</a></li>
+							<li class="menu__item item-4"><a href="#" class="menu__link">Hotel Day Packages</a></li>
+							<li class="menu__item item-5"><a href="#" class="menu__link">Restaurants</a></li>
+							<li class="menu__item item-6"><a href="#" class="menu__link">Events</a></li>
+							<li class="menu__item item-7"><a href="#" class="menu__link">Rodrigues</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+			<!-- .header-menu--mobile -->
+		</header>
+		<section class="breadcrumb">
+			<h2 class="breadcrumb__title">Breadcrumb</h2>
+			<div class="container">
+				<nav class="breadcrumb-nav" aria-label="Page breadcrumb">
+					<ol class="breadcrumb-nav__list">
+						<li class="breadcrumb-nav__item active" aria-current="page"><a href="#" class="breadcrumb-nav__link">Home</a></li>
+						<li class="breadcrumb-nav__item"><a href="#" class="breadcrumb-nav__link">Flash Offers</a></li>
+						<li class="breadcrumb-nav__item">Grand Hilton Hotel</li>
+					</ol>
+				</nav>
+			</div>
+		</section>
+	<!-- /.header -->
